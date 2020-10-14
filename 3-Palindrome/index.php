716833a14027392466e2c720f7394f468eb7d009
Manal-Jaber <manal.jaber94@gmail.com>
2020-10-14 17:04:35 +0300

    fixed 3-palindrome

--- 3-Palindrome/index.php
+++ 3-Palindrome/index.php
@@ -1,12 +1,12 @@
 <?php
 function palindrome($string){
-    $half = Math.floor(strlen($string)/2);
-    if(substr($string, 0, $half ) === strrev(substr($string, (strlen($string)/2)+1,substr($string, 0, strlen($string)) ))){
-        return true;
+    $half = floor(strlen($string)/2);
+    if(substr($string, 0, $half ) === strrev(substr($string, $half+1,strlen($string)))){
+        return "true";
     }else{
-        return false;
+        return "false";
     }
     
 }
-echo palindrome("radar");
+echo palindrome("rof");
 ?>
